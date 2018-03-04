e4abacd4bc8c72c56f2a7c588d33decfa39e1d24
cospamental <cospa18@gmail.com>
2018-03-04 18:57:29 +0300

    added view functions for profile and upload which will render their respective templates

--- instaclone/insta/views.py
+++ instaclone/insta/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Image,Profile
 
 # Create your views here.
 def index(request):
-      return render(request, 'index.html')
+      title = 'Instagrum'
+      image_posts = Image.objects.all()
+      return render(request, 'index.html', {"title":title,"image_posts":image_posts})
+
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    title = 'Instagrum | Profile'
+    profiles = Profile.objects.all()
+    return render(request,'profile.html',{"title":title,"profiles":profiles})
+
+@login_required(login_url='/accounts/login/')
+def upload(request):
+    title = 'Instagrum | Upload'
+    return render(request,'upload.html',{"title":title})
