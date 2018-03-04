572ccc464d4c1d91411ffd374be19fb1bbc2505f
cospamental <cospa18@gmail.com>
2018-03-04 19:02:35 +0300

    added javascript for user interactions

---
+++ instaclone/insta/static/js/main.js
@@ -0,0 +1,17 @@
+// toggle the upload and overlay
+$('#upload-but').click(function() {
+      $('#upload-div').toggle()
+      $('.overlay').toggle()
+  })
+
+$('.caption-but').click(function() {
+      $('#caption-div').slideToggle(200, function() {
+      })
+})
+
+$('.effect').each(function() {
+$(this).click(function() {
+      $('#caption-div').slideUp(200, function() {
+      })    
+})
+})
