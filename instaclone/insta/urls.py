b9e0d3f47628fdb9de48caed5eae570ff8cafe1e
cospamental <cospa18@gmail.com>
2018-03-04 18:56:01 +0300

    added urls for both profile and upload views

--- instaclone/insta/urls.py
+++ instaclone/insta/urls.py
@@ -4,7 +4,9 @@ from django.conf import settings
 from . import views
 
 urlpatterns=[
-      url('^$',views.index, name='index')
+      url('^$',views.index, name='index'),
+      url('^profile/',views.profile, name='profile'),
+      url('^upload/',views.upload, name='upload')
 ]
 
 if settings.DEBUG:
