665c47680f7706b0d58ed745c26a991afb28c6db
cospamental <cospa18@gmail.com>
2018-03-06 01:16:11 +0300

    commented out login redirect url

--- instaclone/instaclone/settings.py
+++ instaclone/instaclone/settings.py
@@ -129,4 +129,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-LOGIN_REDIRECT_URL = 'index'
+# LOGIN_REDIRECT_URL = '/'
