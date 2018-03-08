576f6716642f0534be7549e2ac8133a99f2699a9
cospamental <cospa18@gmail.com>
2018-03-08 23:41:38 +0300

    added the comments url

--- insta/urls.py
+++ insta/urls.py
@@ -5,8 +5,9 @@ from . import views
 
 urlpatterns=[
       url('^$',views.index, name='index'),
-      url('^profile/',views.profile, name='profile'),
+      url('^profile/$',views.profile, name='profile'),
       url('^upload/',views.upload, name='upload'),
+      url('^comments/(?P<id>\d+)',views.post_comment, name='comment'),
       url(r'^search/', views.search_results, name='search_results'),
 ]
 
