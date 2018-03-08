134b184f6375ef1063d5f2642eab7cf9eadc8044
cospamental <cospa18@gmail.com>
2018-03-08 23:42:39 +0300

    added the view function for posting a comment

--- insta/views.py
+++ insta/views.py
@@ -1,25 +1,26 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .models import Image,Profile
-from .forms import EditProfile,UploadForm
+from django.http import HttpResponse,Http404
+from .models import Image,Profile,Comment
+from .forms import EditProfile,UploadForm,CommentForm
 
 # Create your views here.
 def index(request):
-      title = 'Instagrum'
-      image_posts = Image.objects.all()
-      return render(request, 'index.html', {"title":title,"image_posts":image_posts})
+    title = 'Instagrum'
+    image_posts = Image.objects.all()
+    return render(request, 'index.html', {"title":title,"image_posts":image_posts})
 
 
 @login_required(login_url='/accounts/login/')
 def profile(request):
     current_user = request.user
-    title = 'Instagrum | Profile'
+    title = 'Instagrum |Profile'
     profiles = Profile.objects.all()
     return render(request,'profile.html',{"title":title,"profiles":profiles,"user":current_user,})
 
 @login_required(login_url='/accounts/login/')
 def edit_profile(request):
-    title = 'Instagram | Edit'
+    title = 'Instagram |Edit'
     current_user = request.user
     if request.method == 'POST':
         form = EditProfile(request.POST,request.FILES)
@@ -31,24 +32,44 @@ def edit_profile(request):
             return redirect('profile')
     else:
         form = EditProfile()
-    return render(request, 'profile.html', {"title":title, "form":form})
+    return render(request, 'edit_profile.html', {"title":title, "form":form})
 
 @login_required(login_url='/accounts/login/')
 def upload(request):
-    current_user = request.user
-    title = 'Instagrum | Upload'
+    title = 'Instagrum |Upload'
+    current_user = request.user         
     profiles = Profile.get_profile()
     for profile in profiles:
-        form = UploadForm(request.POST,request.FILES)
+        if profile.user.id == current_user.id:
+            if request.method == 'POST':
+                form = UploadForm(request.POST,request.FILES)
+                if form.is_valid():
+                    upload = form.save(commit=False)
+                    upload.user = current_user
+                    upload.profile = profile
+                    upload.save()
+                    return redirect('index')
+            else:
+                form = UploadForm()
+        return render(request,'upload.html',{"title":title, "user":current_user,"form":form})
+
+@login_required(login_url='/accounts/login/')
+def post_comment(request, id):
+    title = 'Instagrum |Comment'
+    post = get_object_or_404(Image, id=id)
+    current_user = request.user
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
         if form.is_valid():
-            upload = form.save(commit=False)
-            upload.user = current_user
-            upload.profile = profile
-            upload.save()
+            post_comment = form.save(commit=False)
+            post_comment.user = current_user
+            post_comment.pic = post
+            post_comment.save()
             return redirect('index')
-        else:
-            form = UploadForm()
-    return render(request,'upload.html',{"title":title, "user":current_user,"form":form})
+    else:
+        form = CommentForm()
+        
+    return render(request,'comments.html',{"title":title,"form":form})
 
 def search_results(request):
 
