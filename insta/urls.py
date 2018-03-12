88900c574186ef2135f9b551e5f287f979a28759
cospamental <cospa18@gmail.com>
2018-03-12 11:28:07 +0300

    appended id on the profile url

--- insta/urls.py
+++ insta/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns=[
       url('^$',views.index, name='index'),
-      url('^profile/$',views.profile, name='profile'),
+      url('^profile/(\d+)',views.profile, name='profile'),
       url('^upload/',views.upload, name='upload'),
       url('^comments/(?P<id>\d+)',views.post_comment, name='comment'),
       url(r'^search/', views.search_results, name='search_results'),
