c9a632f63554e87f86b80a0170e8a72a9d0f81f8
pascaloseko <pascal.ondieki21@gmail.com>
2023-08-08 19:19:58 +0300

    add branch production-3

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ import sys
 def main():
     """Run administrative tasks."""
     # Setting the default Django settings module. Can be overwritten by an environment variable
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'insta.settings.local')  
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'insta.settings.prod')  
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
