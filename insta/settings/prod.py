4bc37da6974a7ae294cec12f7f0113a254af0497
pascaloseko <pascal.ondieki21@gmail.com>
2023-08-08 23:52:05 +0300

    typo

--- insta/settings/prod.py
+++ insta/settings/prod.py
@@ -2,4 +2,4 @@ from .base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://instagrum-xdvsz.ondigitalocean.app', '10.244.5.105']
+ALLOWED_HOSTS = ['instagrum-xdvsz.ondigitalocean.app', '10.244.5.105']
