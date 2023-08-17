6b3bc104b6c2eb81d61b2b08911d3ced78abe8b1
pascaloseko <pascal.ondieki21@gmail.com>
2023-08-17 15:31:22 +0300

    debug

--- manage.py
+++ manage.py
@@ -6,7 +6,12 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    default_settings = "insta.settings.prod"
+    DEBUG = os.environ.get("DJANGO_DEBUG", "False").lower() in ["true", "1"]
+
+    if DEBUG:
+        default_settings = "insta.settings.local"
+    else:
+        default_settings = "insta.settings.prod"
 
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", default_settings)
     try:
